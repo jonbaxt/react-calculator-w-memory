659fc048e13cfe4e36a0d91a220e0c9a22cbf120
jonbaxt <jonbaxt@gmail.com>
2018-09-16 03:25:29 -0600

    Update ReadME, added Redux

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # React Calculator with Online History
+
+- Project Goals:
+
+    - Want to create a website with a calculator that has history connected to a database.
+    - Stack used: REACT, HTML, CSS, Express, Node, PostgreSQL, etc.
+    - Want responsive design and mobile first approach when designing.
+
+- To be done:
+    - Basic Calculator Design.
+    - Create calculator functionality.
+    - Improve CSS and look.
---
+++ src/ducks/reducer.js
@@ -0,0 +1,93 @@
+
+const initialState = {
+    numberDisplayScreen: '0',
+    operatorDisplayScreen: '',
+    currentOperator: '',
+    firstNumber: '',
+    secondNumber: '',
+    solution: '',
+    solutionsArray: []
+}
+
+const SET_NUMBER_DISPLAY_SCREEN = 'SET_NUMBER_DISPLAY_SCREEN';
+const SET_OPERATOR_DISPLAY_SCREEN = 'SET_OPERATOR_DISPLAY_SCREEN';
+const SET_CURRENT_OPERATOR = 'SET_CURRENT_OPERATOR';
+const SET_FIRST_NUMBER = 'SET_FIRST_NUMBER';
+const SET_SECOND_NUMBER = 'SET_SECOND_NUMBER';
+const SET_SOLUTION = 'SET_SOLUTION';
+const ADD_TO_SOLUTIIONS_ARRAY = 'ADD_TO_SOLUTIONS_ARRAY';
+
+const GET_SAVED_SOLUTIONS = 'GET_SAVED_SOLUTIONS';
+
+export function setNumberDisplayScreen(newNumber) {
+    return {
+        type: SET_NUMBER_DISPLAY_SCREEN,
+        payload: newNumber
+    }
+}
+
+export function setOperatorDisplayScreen(newOperator) {
+    return {
+        type: SET_OPERATOR_DISPLAY_SCREEN,
+        payload: newOperator
+    }
+}
+
+export function setCurrentOperator(newOperator) {
+    return {
+        type: SET_CURRENT_OPERATOR,
+        payload: newOperator
+    }
+}
+
+export function setFirstNumber(newNumber) {
+    return { 
+        type: SET_FIRST_NUMBER,
+        payload: newNumber    
+    }
+}
+
+export function setSecondNumber(newNumber) {
+    return {
+        type: SET_SECOND_NUMBER,
+        payload: newNumber
+    }
+}
+
+export function setSolution(newSolution) {
+    return {
+        type: SET_SOLUTION,
+        payload: newSolution
+    }
+}
+
+export function addToSolutionsArray(newSolution) {
+    return {
+        type: ADD_TO_SOLUTIIONS_ARRAY,
+        payload: newSolution
+    }
+}
+
+export function getSavedSolutions() {
+    
+    return 'Will return from the database';
+}
+
+export default function reducer(state = initialState, action) {
+    switch(action.type) {
+        case SET_NUMBER_DISPLAY_SCREEN: 
+            return Object.assign({}, state, { numberDisplayScreen: action.payload });
+        case SET_OPERATOR_DISPLAY_SCREEN:
+            return Object.assign({}, state, { operatorDisplayScreen: action.payload });
+        case SET_CURRENT_OPERATOR:
+            return Object.assign({}, state, { currentOperator: action.payload });
+        case SET_FIRST_NUMBER:
+            return Object.assign({}, state, { firstNumber: action.payload });
+        case SET_SECOND_NUMBER: 
+            return Object.assign({}, state, { secondNumber: action.payload });
+        case SET_SOLUTION:
+            return Object.assign({}, state, { solution: action.payload });
+        case ADD_TO_SOLUTIIONS_ARRAY:
+            return Object.assign({}, state, { solutionsArray: [...solutionsArray, action.payload] });
+    }
+}
